6ee40d7b5280818eb9e4ebe8037b55562d65739e
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-04 23:43:04 -0400

    combined eyes and background

--- background.js
+++ background.js
@@ -1,12 +1,10 @@
+//reference : https://editor.p5js.org/joshgiesbrecht/sketches/iUPHX1UQz
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
   angleMode(DEGREES);
 
-}
-//reference : https://editor.p5js.org/joshgiesbrecht/sketches/iUPHX1UQz
-function draw() {
-  for (let i = 0; i < width; i += 100) {
+    for (let i = 0; i < width; i += 100) {
     for (let j = 0; j < height; j += 100) {
       push();
 
@@ -25,8 +23,31 @@ function draw() {
       pop();
     }
   }
-  
+
 }
+
+// function draw() {
+  // for (let i = 0; i < width; i += 100) {
+  //   for (let j = 0; j < height; j += 100) {
+  //     push();
+
+  //     translate(i+50, j);
+
+  //     noStroke();
+  //     fill("#8EAB62")
+  //     for (let k = 0; k < 6; k++) {
+  //       rotate(60);
+  //       ellipse(0, -5, 15, 35);
+  //     }
+
+  //     fill("#6F914A")
+  //     circle(0, 0, 15);
+
+  //     pop();
+  //   }
+  // }
+  
+//}
 
 
 
--- combined.js
+++ combined.js
@@ -1,5 +1,28 @@
 function setup() {
-    createCanvas(windowWidth, wind);
+    createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES)
+
+    for (let i = 0; i < width; i += 100) {
+        for (let j = 0; j < height; j += 100) {
+        push();
+
+        translate(i+50, j+150);
+
+        noStroke();
+        fill("#8EAB62")
+        for (let k = 0; k < 6; k++) {
+            rotate(60);
+            ellipse(0, -5, 15, 35);
+        }
+
+        fill("#6F914A")
+        circle(0, 0, 15);
+
+        pop();
+    }
+  }
+
+
 
 }
 
